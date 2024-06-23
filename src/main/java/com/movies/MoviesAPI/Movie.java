25be0da0cbe057621cda579e43d722cfaa7f2d5c
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-06-23 08:51:23 +0300

    movies API

--- src/main/java/com/movies/MoviesAPI/Movie.java
+++ src/main/java/com/movies/MoviesAPI/Movie.java
@@ -1,4 +1,21 @@
 package com.movies.MoviesAPI;
 
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "movies")
+// the annotation Document tells the compiler that this file is for creating a document in the database
 public class Movie {
+
+//    over here we will have the private datas
+
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailer_link;
+
+
+
 }
